20faed513f821bcb847367ab0373f176c4050fe7
nitin <ngi563980@gmail.com>
2022-01-14 22:20:00 +0530

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const path = require('path');
 const nodemailer = require('nodemailer');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'));
-app.use('/static',express.static('static'));
+app.use('/static', express.static('static'));
 app.use(express.urlencoded());
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/html/index.html');
 });
-app.post('/contact',(req, res)=>{
+app.post('/contact', (req, res) => {
   name = req.body.name
   email = req.body.email
   num = req.body.num
   sub = req.body.sub
   t = req.body.t
-  
+
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -26,27 +27,38 @@ app.post('/contact',(req, res)=>{
       pass: 'nitin12@'
     }
   });
-  
+
   var mailOptions = {
     from: 'ngi563980@gmail.com',
     to: req.body.email,
     subject: `${sub}`,
     text: `hello I am ${name} [email : ${email} , Number : ${num}] and ${t}`
   };
-  
+
   transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-  
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+
   });
-  
-  res.render('contact',{n:`${name}`,nu:`${num}`,e:`${email}`})
+  const file = JSON.stringify(req.body);
+  fs.writeFileSync('data.json', file, (err) => {
+    console.log(err);
+  })
+
+  res.render('contact', { n: `${name}`, nu: `${num}`, e: `${email}` });
 });
 
-app.listen(port,()=>{
+app.get('/app',(req, res)=>{
+  const re = fs.readFileSync('data.json',(err)=>{
+    console.log(err);
+  });
+  const r = JSON.parse(re);
+  res.render('app', { n: r.name , nu: r.num , e: r.email,s: r.sub,te: r.t });
+});
+
+app.listen(port, () => {
   console.log('hello I am working ');
 });
-
