0469568725a8bd80cac2298f51fce527a0e075a1
nitin <ngi563980@gmail.com>
2022-01-14 08:53:33 +0530

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/static',express.static('static'))
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'));
+app.use('/static',express.static('static'));
+app.use(express.urlencoded());
+
 app.get('/',(req, res)=>{
   res.sendFile(__dirname + '/html/index.html');
 });
+app.post('/contact',(req, res)=>{
+  name = req.body.name
+  num = req.body.num
+  email = req.body.email
+  res.render('contact',{n:`${name}`,nu:`${num}`,e:`${email}`})
+})
 
 app.listen(port,()=>{
   console.log('hello I am working ');
